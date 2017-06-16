0d90d9604caae7dad803eec1f19c9886ba0facf3
Natan Facchin <maxmousee@gmail.com>
2017-06-15 21:44:11 -0300

    Added unit tests

--- app/src/test/java/com/nfsindustries/androidtasks/CommonUtilsTest.java
+++ app/src/test/java/com/nfsindustries/androidtasks/CommonUtilsTest.java
@@ -1,17 +1,42 @@
 package com.nfsindustries.androidtasks;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import com.nfsindustries.androidtasks.utils.CommonUtils;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * CommonUtils unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class CommonUtilsTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void notNullCommonUtilsTest() throws Exception {
+        Context context = mock(Context.class);
+        Activity activity = mock(Activity.class);
+        CommonUtils commonUtils = new CommonUtils(context, activity);
+        assertNotNull(commonUtils);
+    }
+
+    @Test
+    public void deviceOfflineTest() throws Exception {
+        Context context = mock(Context.class);
+        Activity activity = mock(Activity.class);
+        ConnectivityManager connMgr = mock(ConnectivityManager.class);
+        when(activity.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(connMgr);
+        CommonUtils commonUtils = new CommonUtils(context, activity);
+        assertFalse(commonUtils.isDeviceOnline());
     }
 }
